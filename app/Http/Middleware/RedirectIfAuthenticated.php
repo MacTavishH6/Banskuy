cacfa55d0c915b6be0bd1ff18aeed96956502f01
Muhammad Rafli Rosliana <72242271+raflirosliana@users.noreply.github.com>
2021-11-15 17:23:12 +0700

    15 Nov 2021 - Penambahan Auth Auth-an Jilid 2

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,7 +22,9 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
+            
             if (Auth::guard($guard)->check()) {
+                dd("PEDO!");
                 return redirect(RouteServiceProvider::HOME);
             }
         }
