7511024e63eb9d3a184cb8632814cac1f69a6598
M Wira Nugraha <m.nugraha@binus.edu>
2021-11-28 14:46:52 +0700

    Adding favicon

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,55 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Banskuy.com</title>
-
+@extends('layouts.app')
+@section('styles')
     <style>
-        .backgroundstyle{
-            background-image: url(<?php echo env('FTP_URL')."assets/lp_background.jpg" ?>);
+        .backgroundstyle {
+            background-image: url(<?php echo env('FTP_URL') . 'assets/lp_background.jpg'; ?>);
             background-repeat: no-repeat;
             background-size: cover;
         }
-        .backgroundcolor1{
+
+        .backgroundcolor1 {
             /* background-color:#18171f;  */
             border-radius: 50px;
             padding-bottom: 3%;
         }
-        .buttonlp{
-            border-radius: 20px; 
-            background-color: #9F51CF; 
-            border: none; 
+
+        .buttonlp {
+            border-radius: 20px;
+            background-color: #9F51CF;
+            border: none;
             font-size: 140%;
-            transition:0.2s;
+            transition: 0.2s;
         }
-        .buttonlp:hover{
+
+        .buttonlp:hover {
             background-color: #45C1A4;
-            box-shadow: 0px 0px 20px rgb(255,255,255);
+            box-shadow: 0px 0px 20px rgb(255, 255, 255);
         }
+
         .card {
-            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
             transition: 0.3s;
             width: 40%;
         }
+
         .card:hover {
-            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
         }
+
         .container {
             padding: 2px 16px;
         }
-        .donationstuffcontainer{
+
+        .donationstuffcontainer {
             margin-left: 5%;
             margin-right: 5%;
         }
+
     </style>
+@endsection
 
-</head>
-<body>
-    @extends('layouts.app')
-
-    @section('content')
+@section('content')
     <div class="backgroundstyle">
         <div class="backgroundcolor1 d-flex">
             <div class="col-6 text-center" style="color: white">
@@ -85,32 +84,38 @@
 
         <div class="donationstuffcontainer d-flex">
             <div class="card" style="margin:1%; background-color: #18171f; border-radius: 20px">
-                <img class="text-center" src="{{env("FTP_URL")}}assets/uang.png" alt="image" style="max-width:200px; color: white">
+                <img class="text-center" src="{{ env('FTP_URL') }}assets/uang.png" alt="image"
+                    style="max-width:200px; color: white">
                 <div class="container text-center" style="color: white">
                     <h2><b>Uang</b></h2> <br>
-                    <p style="font-size: 140%">Anda dapat mendonasikan uang anda dengan mentranfser uang and ke rekening Yayasan atau langsung.</p> 
+                    <p style="font-size: 140%">Anda dapat mendonasikan uang anda dengan mentranfser uang and ke rekening
+                        Yayasan atau langsung.</p>
                     <br><br>
                 </div>
             </div>
             <div class="card" style="margin:1%; background-color: #18171f; border-radius: 20px">
-                <img class="text-center" src="{{env("FTP_URL")}}assets/barang.png" alt="image" style="max-width:200px; color: white">
+                <img class="text-center" src="{{ env('FTP_URL') }}assets/barang.png" alt="image"
+                    style="max-width:200px; color: white">
                 <div class="container text-center" style="color: white">
                     <h2><b>Barang Bekas</b></h2> <br>
-                    <p style="font-size: 140%">Anda memiliki barang bekas dalam keadaan masih layak pakai ? Kamu dapat mendonasikannya, sederhana namun sangat berharga</p> 
+                    <p style="font-size: 140%">Anda memiliki barang bekas dalam keadaan masih layak pakai ? Kamu dapat
+                        mendonasikannya, sederhana namun sangat berharga</p>
                     <br><br>
                 </div>
             </div>
             <div class="card" style="margin:1%; background-color: #18171f; border-radius: 20px">
-                <img class="text-center" src="{{env("FTP_URL")}}assets/comserv.png" alt="image" style="max-width:200px; color: white">
+                <img class="text-center" src="{{ env('FTP_URL') }}assets/comserv.png" alt="image"
+                    style="max-width:200px; color: white">
                 <div class="container text-center" style="color: white">
                     <h2><b>Pelayanan Masyarakat</b></h2> <br>
-                    <p style="font-size: 140%">Menolong orang lain adalah hal yang sederhana, jika anda memiliki waktu luang, anda dapat menolong mereka.</p> 
+                    <p style="font-size: 140%">Menolong orang lain adalah hal yang sederhana, jika anda memiliki waktu
+                        luang, anda dapat menolong mereka.</p>
                     <br><br>
                 </div>
             </div>
         </div>
-        
-        <br><br> 
+
+        <br><br>
 
         {{-- SLIDERS START HERE --}}
         <div class="slider">
@@ -125,9 +130,7 @@
         </div>
 
         <div>
-            
+
         </div>
     </div>
-    @endsection        
-</body>
-</html>
+@endsection
