45c6ea5ab04e38ecec199995035d08bf38eebe40
M Wira Nugraha <m.nugraha@binus.edu>
2021-12-18 11:56:43 +0700

    18Des21 Wira - change order by in history

--- resources/views/Transaction/transactionapproval.blade.php
+++ resources/views/Transaction/transactionapproval.blade.php
@@ -84,29 +84,7 @@
     <div id="list-containter">
 
     </div>
-    <div class="paginationhistory d-flex justify-content-around mt-5 mb-5">
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                </li>
-
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+    
 </div>
 <div class="modal-containter">
 
